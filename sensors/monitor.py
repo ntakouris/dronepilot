69fb801b8e323e8082a90444fa7a9df86117061c
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2018-11-26 17:26:12 +0200

    Add serial and parallel poll functions

--- sensors/monitor.py
+++ sensors/monitor.py
@@ -1,6 +1,72 @@
+import threading
+from threading import Thread
+
 import serial
 import time
+
 from sensors.accelerometer import Accelerometer
+
+''' Poll serially sensor list ordered ascending by their measurement delay'''
+
+
+def serial_poll(sensors, measure_tick_event, kill_event):
+    sensors.sort(key=lambda e: e.get_delay_between_measurements)
+
+    measure_time_map = {}
+
+    now = time.time()
+
+    for sensor in sensors:
+        measure_time_map[sensor] = now - sensor.get_delay_between_measurements()
+
+    while not kill_event.is_set():
+        for sensor in sensors:
+            now = time.time()
+            last_measure = measure_time_map[sensor]
+            diff = now - last_measure
+
+            if diff < sensor.get_delay_between_measurements():
+                time.sleep(sensor.get_delay_between_measurements() - diff)
+
+            print(f'Polling {type(sensor).__name__}')
+            sensor.poll_measure()
+            print(f'Poll ended at: {time.time()}')
+        measure_tick_event.set()
+
+
+'''Poll all sensors in different threads, in parallel'''
+
+
+def parallel_poll(sensors, measure_tick_event, kill_event):
+    def _poll(sensor, completed_event):
+        while not kill_event.is_set():
+            sensor.poll_measure()
+            completed_event.set()
+            time.sleep(sensor.get_delay_between_measurements())
+
+    time_wait_quantum = min(sensors, lambda s: s.get_delay_between_measurements())
+
+    threads = []
+    events = []
+
+    for sensor in sensors:
+        e = threading.Event()
+        t = Thread(target=_poll(sensor, e))
+        events.append(e)
+        threads.append(t)
+
+    for thread in threads:
+        thread.start()
+
+    def _set_and_clear(event):
+        event.wait()
+        event.clear()
+
+    while not kill_event.is_set():
+        time.sleep(time_wait_quantum)
+        [_set_and_clear(e) for e in events]
+        print(f'Parallel poll ended at: {time.time()}')
+        measure_tick_event.set()
 
 
 def main():
@@ -21,6 +87,8 @@ def main():
                 sensor.poll_measure()
                 print(f'Ended at: {time.time()}')
     except KeyboardInterrupt:
+        for sensor in sensors:
+            sensor.dispose()
         pass
 
 
