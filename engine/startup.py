2e93578b4e3730e18218cd16e7b1c2990d8cd18a
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2018-12-01 00:27:20 +0200

    Initial for estimation engine

---
+++ engine/startup.py
@@ -0,0 +1,13 @@
+import threading
+
+
+def main():
+    kill_event = threading.Event()
+    predict_event = threading.Event()
+    update_event = threading.Event()
+
+    sensor_list = []
+
+
+if __name__ == '__main__':
+    main()
