c0e62f235a55a80681a6da5ee18ff61fcd35fd3f
Zarkopafilis <zarkopafilis@gmail.com>
2018-12-01 15:37:03 +0200

    Add some metrics to polling algorithms

--- sensors/poll.py
+++ sensors/poll.py
@@ -17,17 +17,22 @@ def poll_sequential_smart(sensors, measure_tick_event, kill_event):
         measure_time_map[sensor] = now - sensor.get_delay_between_measurements()
 
     while not kill_event.is_set():
+        start = time.time()
         for sensor in sensors:
             now = time.time()
             last_measure = measure_time_map[sensor]
             diff = now - last_measure
 
-            if diff < sensor.get_delay_between_measurements():
-                time.sleep((sensor.get_delay_between_measurements() - diff) / 1000)
-
             print(f'Polling {type(sensor).__name__}')
+
+            if diff < sensor.get_delay_between_measurements():
+                _del = sensor.get_delay_between_measurements() - diff
+                print(f'Sleeping for extra {_del / 1000} ms')
+                time.sleep(_del / 1000)
+
             sensor.poll_measure()
-            print(f'Poll ended at: {time.time()}')
+            print(f'Poll ended')
+        print(f'Finished polling after {(time.time() - start) / 1000} ms')
         measure_tick_event.set()
 
 
@@ -37,7 +42,12 @@ def poll_sequential_smart(sensors, measure_tick_event, kill_event):
 def poll_parallel(sensors, measure_tick_event, kill_event):
     def _poll(_sensor, completed_event):
         while not kill_event.is_set():
+            print(f'Polling {type(sensor).__name__}')
+            _start = time.time()
             _sensor.poll_measure()
+            print(f'Finished polling {type(sensor).__name__} after {(time.time() - _start) / 1000} ms')
+            _start = time.time()
+
             completed_event.set()
             time.sleep(_sensor.get_delay_between_measurements() / 1000)
 
@@ -52,6 +62,7 @@ def poll_parallel(sensors, measure_tick_event, kill_event):
         events.append(e)
         threads.append(t)
 
+    now = time.time()
     for thread in threads:
         thread.start()
 
@@ -62,7 +73,8 @@ def poll_parallel(sensors, measure_tick_event, kill_event):
     while not kill_event.is_set():
         time.sleep(time_wait_quantum)
         [_set_and_clear(e) for e in events]
-        print(f'Parallel poll ended at: {time.time()}')
+        print(f'Parallel poll ended after: {(time.time() - now) / 1000} ms')
+        now = time.time()
         measure_tick_event.set()
 
     [t.join() for t in threads]
@@ -73,9 +85,13 @@ def poll_parallel(sensors, measure_tick_event, kill_event):
 
 def poll_sequential_dumb(sensors, measure_tick_event, kill_event):
     while not kill_event.is_set():
+        start = time.time()
         for sensor in sensors:
             print(f'Polling {type(sensor).__name__}')
+            _start = time.time()
             sensor.poll_measure()
-            print(f'Poll ended at: {time.time()}')
+            print(f'Poll ended after: {(time.time() - start) / 1000} ms')
         measure_tick_event.set()
+        print(f'Polled all sensors after {(time.time() - start) / 1000} ms')
+        start = time.time()
 
